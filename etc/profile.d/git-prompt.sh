0996062739d173dc61f50c8d92cd45891f932ce1
PR271F5N <PR271F5N@atlas.edf.fr>
2016-03-15 13:39:12 +0100

    Modification de etc/profile.d/git-prompt.sh pour placer le Homedir dans ~

--- etc/profile.d/git-prompt.sh
+++ etc/profile.d/git-prompt.sh
@@ -5,6 +5,7 @@ then
 else
 	TITLEPREFIX=$MSYSTEM
 fi
+cd ~
 PS1='\[\033]0;$TITLEPREFIX:${PWD//[^[:ascii:]]/?}\007\]' # set window title
 #PS1="$PS1"'\n'                 # new line
 PS1="$PS1"'\[\033[32m\]'       # change to green
