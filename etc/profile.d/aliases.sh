6ef2151d7529451829a1b7afc152e00376da8e50
Typikall <PR271F5N@atlas.edf.fr>
2016-05-10 10:39:16 +0200

    Modification du chemin des scripts

--- etc/profile.d/aliases.sh
+++ etc/profile.d/aliases.sh
@@ -23,19 +23,18 @@ esac
 # Alias commande
 #==============
 # Alias commande
-alias ssh='ssh -l PR271F5N'
-alias lssip='cat $HOME/prakotoarisoa/sipsir/list_sipsir'
-alias lscac2='cat $HOME/prakotoarisoa/caccia/list_caccia2'
-alias cacverif='$HOME/prakotoarisoa/caccia/./cac_verif.sh'
-alias sipverif='$HOME/prakotoarisoa/sipsir/./sip_verif.sh'
-#alias ssh='ssh() {echo -ne "\033]0;${@: -1}\007";exec ssh $@}'
-alias ls='ls --color=auto'
-# Alias acces serveur
-alias noe921rs='ssh admsrv@noe921rs'
-alias noeyyxhu='ssh admsrv@noeyyxhu'
-alias noeyyxhv='ssh admsrv@noeyyxhv'
-alias pcyyj10p6='ssh admsrv@pcyyj10p6'
-alias noeyyxj6='ssh admsrv@noeyyxj6'
-alias clay71avm='ssh admsrv@clay71avm'
-alias clay73mvm='ssh root@clay73mvm'
-
+#alias ssh='ssh -l PR271F5N'
+#alias lssip='cat $HOME/prakotoarisoa/sipsir/list_sipsir'
+#alias lscac2='cat $HOME/prakotoarisoa/caccia/list_caccia2'
+#alias cacverif='$HOME/prakotoarisoa/caccia/./cac_verif.sh'
+#alias sipverif='$HOME/prakotoarisoa/sipsir/./sip_verif.sh'
+##alias ssh='ssh() {echo -ne "\033]0;${@: -1}\007";exec ssh $@}'
+#alias ls='ls --color=auto'
+## Alias acces serveur
+#alias noe921rs='ssh admsrv@noe921rs'
+#alias noeyyxhu='ssh admsrv@noeyyxhu'
+#alias noeyyxhv='ssh admsrv@noeyyxhv'
+#alias pcyyj10p6='ssh admsrv@pcyyj10p6'
+#alias noeyyxj6='ssh admsrv@noeyyxj6'
+#alias clay71avm='ssh admsrv@clay71avm'
+#alias clay73mvm='ssh root@clay73mvm'
